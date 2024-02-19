1a7f9621dd0d9b4c8bcf3398ea6ef0d7ad3e4c45
Alli_Eunbi <allieunbi@gmail.com>
2024-02-19 18:14:03 +0900

    feat: app pvc 연동하여 30G 연동

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,6 @@ provider "aws" {
   region = local.region
 }
 
-# data "aws_caller_identity" "current" {}
-
 locals {
   name    = "nsus-cluster"
 #   todo: cluster verison 확인
@@ -30,7 +28,6 @@ locals {
 ################################################################################
 # VPC Module
 ################################################################################
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
 
@@ -90,13 +87,7 @@ module "eks" {
   version = "20.2.1"
 
   cluster_name                   = local.name
-#   cluster_version                = local.cluster_version
-# 실제 운영 환경이라면 cluter_endpoint_public_access를 false 처리하고 vpn 내부 접근으로 변경할것
   cluster_endpoint_public_access = true
-
-  # IPV6
-  # cluster_ip_family          = "ipv6"
-  # create_cni_ipv6_iam_policy = true
 
   enable_cluster_creator_admin_permissions = true
 
@@ -111,11 +102,11 @@ module "eks" {
       most_recent    = true
       before_compute = true
     }
-    aws-ebs-csi-driver = {
-      # service_account_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.name}-ebs-csi-controller"
-      service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
-    }
-
+    # aws-ebs-csi-driver = {
+    #   # service_account_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.name}-ebs-csi-controller"
+    #   service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
+    #   resolve_conflicts        = "OVERWRITE"
+    # }
   }
 
   vpc_id                   = module.vpc.vpc_id
@@ -124,7 +115,7 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["t2.micro", "t3.medium"]
+    instance_types = ["t3.medium"]
 
   }
 
@@ -134,29 +125,9 @@ module "eks" {
       max_size = 3
       desired_size = 2
       disk_size = 20
-
-      # Remote access cannot be specified with a launch template
-      # ISMS 보안 정책 위배
-      # remote_access = {
-      #   ec2_ssh_key               = module.key_pair.key_pair_name
-      #   source_security_group_ids = [aws_security_group.remote_access.id]
-      # }
     }
     tags = local.tags
   }
-
-  # create_iam_role          = true
-  # iam_role_name            = "iam-managed-node-group"
-  # iam_role_use_name_prefix = false
-  # iam_role_description     = "EKS managed node group complete example role"
-  # iam_role_tags = {
-  #   Purpose = "Protector of the kubelet"
-  # }
-  # iam_role_additional_policies = {
-  #   AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  #   additional                         = aws_iam_policy.node_additional.arn
-  # }
-      
 }
 
 
@@ -225,14 +196,6 @@ module "iam_github_oidc_role" {
   }
 }
 
-# module "iam_github_oidc_role_disabled" {
-#   source = "terraform-aws-modules/iam/aws//examples/iam-github-oidc"
-
-#   create = false
-# }
-
-
-
 ################################################################################
 # ECR
 ################################################################################
@@ -244,11 +207,6 @@ resource "aws_ecr_repository" "nsus-ecr" {
   }
 }
 
-# resource "aws_ecr_repository_policy" "nsus_ecr_policy" {
-#   repository = aws_ecr_repository.nsus-ecr.name
-#   policy     = aws_iam_policy.ECR_Read_Write.policy.json
-# }
-
 output "ecr_registry_id" {
   value = aws_ecr_repository.nsus-ecr.registry_id
 }
@@ -257,57 +215,59 @@ output "ecr_repository_url" {
   value = aws_ecr_repository.nsus-ecr.repository_url
 }
 
-################################################################################
-# RDS Module
-################################################################################
-module "db" {
-  source = "terraform-aws-modules/rds/aws"
+# ################################################################################
+# # RDS Module
+# ################################################################################
+# module "db" {
+#   source = "terraform-aws-modules/rds/aws"
 
-  identifier = "${local.name}-default"
+#   identifier = "${local.name}-default"
 
-  create_db_option_group    = false
-  create_db_parameter_group = false
+#   create_db_option_group    = false
+#   create_db_parameter_group = false
 
-  # All available versions: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MySQL.html#MySQL.Concepts.VersionMgmt
-  engine               = "mysql"
-  engine_version       = "8.0"
-  family               = "mysql8.0" # DB parameter group
-  major_engine_version = "8.0"      # DB option group
-  instance_class       = "db.t4g.micro"
+#   # All available versions: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MySQL.html#MySQL.Concepts.VersionMgmt
+#   engine               = "mysql"
+#   engine_version       = "8.0"
+#   family               = "mysql8.0" # DB parameter group
+#   major_engine_version = "8.0"      # DB option group
+#   instance_class       = "db.t4g.micro"
 
-  allocated_storage = 200
+#   allocated_storage = 200
 
-  db_name  = "nsus"
-  username = "admin"
-  port     = 3306
+#   db_name  = "nsus"
+#   username = "admin"
+#   port     = 3306
+
+#   manage_master_user_password = false
+#   password = "admin1234"
+
+#   multi_az               = false
+#   db_subnet_group_name   = module.vpc.database_subnet_group
+#   vpc_security_group_ids = ["sg-030c6835659768139"]
+#   create_cloudwatch_log_group     = false
+#   publicly_accessible = true
+
+#   skip_final_snapshot = true
+#   deletion_protection = false
+
+#   performance_insights_enabled          = false
+#   create_monitoring_role                = false
 
 
-  multi_az               = false
-  db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.security_group.security_group_id]
-  create_cloudwatch_log_group     = false
-  publicly_accessible = true
+#   parameters = [
+#     {
+#       name  = "character_set_client"
+#       value = "utf8mb4"
+#     },
+#     {
+#       name  = "character_set_server"
+#       value = "utf8mb4"
+#     }
+#   ]
 
-  skip_final_snapshot = true
-  deletion_protection = false
-
-  performance_insights_enabled          = false
-  create_monitoring_role                = false
-
-
-  parameters = [
-    {
-      name  = "character_set_client"
-      value = "utf8mb4"
-    },
-    {
-      name  = "character_set_server"
-      value = "utf8mb4"
-    }
-  ]
-
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 5.0"
@@ -323,7 +283,16 @@ module "security_group" {
       to_port     = 3306
       protocol    = "tcp"
       description = "MySQL access from within VPC"
-      cidr_blocks = module.vpc.vpc_cidr_block
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+    egress_with_cidr_blocks = [
+    {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      description = "MySQL access from within VPC"
+      cidr_blocks = "0.0.0.0/0"
     },
   ]
 
